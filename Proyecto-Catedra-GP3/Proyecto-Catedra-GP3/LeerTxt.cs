435653cfde75c3f0edd2bb0291fc8f54ebad1106
Bryan Josue Alberto Elena <bryanelena@outlook.com>
2021-04-09 15:15:49 -0600

    Agrego la lectura del txt

---
+++ Proyecto-Catedra-GP3/Proyecto-Catedra-GP3/LeerTxt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proyectoPrueba
+{
+    class LeerTxt
+    {
+        public static string[,] LeerTexto()
+        {
+            int counter = 0;
+            string line;
+            string[,] valores = new string[10, 5];
+            string[] vector = new string[5];
+
+            System.IO.StreamReader leerTxt =
+                new System.IO.StreamReader(@"C:\Users\bryan\Desktop\Proyecto Cátedra\Avance 2\proyectoPrueba\Inventario.txt"); // Ubicación que debe sustituir
+            while ((line = leerTxt.ReadLine()) != null)
+            {
+                vector = line.Split(","); //Separa la linea cuando exista una coma
+                for (int i = 0; i < vector.Length; i++) //i = columna
+                {
+                    //Console.WriteLine(vector[i]);
+                    valores[counter, i] = vector[i];
+                }
+                counter++; //Fila
+            }
+            leerTxt.Close();
+            return valores;
+
+        }
+
+    }
+}
